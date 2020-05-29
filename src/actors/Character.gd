aa20b3651ae317843aa69a490b8f4913a9cc08c4
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-05-29 18:02:47 -0400

    fix

--- src/actors/Character.gd
+++ src/actors/Character.gd
@@ -8,12 +8,11 @@ export var reaction_speed = 17.5
 export var rotation_speed = 12.5
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+#	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
 
 func _process(delta):
 	center_pos = Vector2(center_pos.x + 1, center_pos.y)
-	var relative_pos = position - center_pos
 	var mouse_pos = get_global_mouse_position()
-	relative_pos = relative_pos.linear_interpolate(mouse_pos, delta * reaction_speed)
-	position = center_pos + relative_pos
-	sprite.rotation_degrees = lerp(sprite.rotation_degrees, clamp(mouse_pos.y - relative_pos.y, -35, 35), delta * rotation_speed)
+	position = position.linear_interpolate(mouse_pos, delta * reaction_speed)
+	sprite.rotation_degrees = lerp(sprite.rotation_degrees, clamp(mouse_pos.y - position.y, -35, 35), delta * rotation_speed)
