808e51795b9c2952bcf5c900378505380ebffacd
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-05-29 22:40:16 +0200

    add torpedo controls

--- src/Actor.gd
+++ src/Actor.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
+export var reaction_speed = 17.5
+export var rotation_speed = 12.5
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var mouse_pos = get_global_mouse_position()
+	position = position.linear_interpolate(mouse_pos, delta * reaction_speed)
+	rotation_degrees = lerp(rotation_degrees, clamp(mouse_pos.y - position.y, -35, 35), delta * rotation_speed)
