8d0062481c3098e36bded9c558944a4e76e47b4b
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-05-30 10:05:38 +0200

    make better path

--- src/actors/Character.gd
+++ src/actors/Character.gd
@@ -14,6 +14,7 @@ export var path_interpolation_speed = 15
 export var path : NodePath
 var path_points
 var target
+var move_normal = Vector2()
 
 var base_rotation = 0
 
@@ -23,12 +24,15 @@ func _ready():
 	if path:
 		path_node.curve.set_bake_interval(fly_speed)
 		path_points = path_node.curve.get_baked_points()
+		path_node.curve.tessellate(5, 4)
 	target = path_points[path_index]
+	move_normal = (target - center_pos).normalized()
 		
 func _physics_process(delta):
-	if center_pos.distance_to(target) < fly_speed:
-		path_index = clamp(path_index + 1, 0, path_points.size() - 1)
+	if center_pos.distance_to(target) < fly_speed and path_index + 1 < path_points.size():
+		path_index += 1
 		target = path_points[path_index]
+		move_normal = (target - center_pos).normalized()
 		
 	if path_index > 0:
 		var last_target = path_points[path_index - 1]
@@ -38,7 +42,7 @@ func _physics_process(delta):
 		var y_normal = Vector2(dy, -dx)
 		base_rotation = y_normal.angle() + PI/2
 		
-	center_pos += (target - center_pos).normalized() * fly_speed
+	center_pos += move_normal * fly_speed
 		
 	var mouse_pos = get_global_mouse_position()
 	position = position.linear_interpolate(mouse_pos, delta * reaction_speed)
