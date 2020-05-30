416320a23db2eb21eb36cc2b49b2cdc9b22ba080
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-05-30 08:22:29 +0200

    add rotation

--- src/Camera2D.gd
+++ src/Camera2D.gd
@@ -4,5 +4,8 @@ export var character : NodePath
 
 onready var character_node = get_node(character)
 
-func _process(delta):
+var rotation_speed = 1
+
+func _physics_process(delta):
 	position = character_node.center_pos
+	rotation = lerp_angle(rotation, character_node.base_rotation, delta * rotation_speed)
--- src/actors/Character.gd
+++ src/actors/Character.gd
@@ -8,12 +8,14 @@ onready var path_node = get_node(path)
 
 export var reaction_speed = 17.5
 export var fly_speed = 5
-export var rotation_speed = 12.5
+export var rotation_speed = 1
 
 export var path_interpolation_speed = 15
 export var path : NodePath
 var path_points
 var target
+
+var base_rotation = 0
 
 func _ready():
 	center_pos = position
@@ -27,8 +29,17 @@ func _physics_process(delta):
 		path_index = clamp(path_index + 1, 0, path_points.size() - 1)
 		target = path_points[path_index]
 		
+	if path_index > 0:
+		var last_target = path_points[path_index - 1]
+		var dx = target.x - last_target.x
+		var dy = target.y - last_target.y
+		var x_normal = Vector2(-dy, dx)
+		var y_normal = Vector2(dy, -dx)
+		base_rotation = y_normal.angle() + PI/2
+		
 	center_pos += (target - center_pos).normalized() * fly_speed
 		
 	var mouse_pos = get_global_mouse_position()
 	position = position.linear_interpolate(mouse_pos, delta * reaction_speed)
-	sprite.rotation_degrees = lerp(sprite.rotation_degrees, clamp(mouse_pos.y - position.y, -30, 30), delta * rotation_speed)
+	sprite.rotation = lerp(sprite.rotation, base_rotation, delta * rotation_speed)
+	#lerp(sprite.rotation_degrees, clamp(mouse_pos.y - position.y, -30, 30), delta * rotation_speed) + rad2deg(base_rotation)
