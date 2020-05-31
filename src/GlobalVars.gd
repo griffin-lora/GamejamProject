ab9895161ac98226095583964ce75ef4ff0e8f3a
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-05-30 22:41:55 -0400

    add ability stuff sorta

--- src/GlobalVars.gd
+++ src/GlobalVars.gd
@@ -3,6 +3,20 @@ extends Node
 var checkpoint_id := 0
 var level_id := 0
 
+# RECHARGE TIME
+var ability_recharge_time : float = 5
+
+# Internal
+var ability_recharge_ct : float = 0
+
+func _ready():
+	ability_recharge_ct = ability_recharge_time
+	
+func _process(delta):
+	ability_recharge_ct = ability_recharge_ct + delta
+	if ability_recharge_ct >= ability_recharge_time and Input.is_action_just_pressed("use_ability"):
+		activate_ability()
+
 func reset():
 	checkpoint_id = 0
 
@@ -11,3 +25,7 @@ func switch_level():
 	var level_list = load("res://level_list.tres")
 	var level_name = level_list.levels[level_id]
 	get_tree().change_scene("res://levels/" + level_name + ".tscn")
+
+func activate_ability():
+	ability_recharge_ct = 0
+	print("ABILITY ACTIVATED")
