6e8b277e2b99e38da58cfd67f41a437858b46ad4
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2020-06-02 20:51:26 +0200

    Add question block

--- src/ObstacleButton.gd
+++ src/ObstacleButton.gd
@@ -5,6 +5,12 @@ export var editor_path : NodePath
 
 onready var editor = get_node(editor_path)
 
+func _process(delta):
+	if is_hovered() and !pressed:
+		modulate = Color(0.85, 0.85, 0.95)
+	else:
+		modulate = Color(1, 1, 1)
+
 func _pressed():
 	editor.selected_obstacle = obstacle_id
 	editor.update_selected_obstacle()
