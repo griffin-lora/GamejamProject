2b759d4646bd22a589712d6c7fbd5d752388b25c
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-06-02 19:26:33 -0400

    tried smoohting it faildd

--- src/Path2DVisualizer.gd
+++ src/Path2DVisualizer.gd
@@ -4,6 +4,28 @@ extends Path2D
 
 var sprites = []
 
+var spline_length := 100
+
+func _get_spline(i):
+	var last_point = _get_point(i - 1)
+	var next_point = _get_point(i + 1)
+	var spline = last_point.direction_to(next_point) * spline_length
+	return spline
+
+func _get_point(i):
+	var point_count = curve.get_point_count()
+	i = wrapi(i, 0, point_count - 1)
+	return curve.get_point_position(i)
+
+func smooth(value):
+	if not value: return
+
+	var point_count = curve.get_point_count()
+	for i in point_count:
+		var spline = _get_spline(i)
+		curve.set_point_in(i, -spline)
+		curve.set_point_out(i, spline)
+
 func add_point(p, add_to_data = false, v = Vector2(), v2 = Vector2()):
 	curve.add_point(p, v, v2)
 	if add_to_data:
@@ -19,6 +41,7 @@ func add_point(p, add_to_data = false, v = Vector2(), v2 = Vector2()):
 	sprite.offset = (Vector2(sprite.margin_right, sprite.margin_bottom) / 2)
 	sprite.rect_position = p - sprite.offset
 	add_child(sprite)
+#	smooth(100) haha if this worked
 	update()
 
 func _ready():
