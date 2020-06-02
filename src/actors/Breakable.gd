e86fd68d2b57e140622eba805dfdde17cd7495b9
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-06-02 11:15:30 -0400

    this is some bullshit

--- src/actors/Breakable.gd
+++ src/actors/Breakable.gd
@@ -1,17 +1,21 @@
 extends Actor
+class_name Breakable
 
 onready var area = $Area2D
+onready var area_collision = $Area2D/CollisionShape2D
+onready var editor_area = $EditorArea
+onready var editor_collision = $EditorArea/CollisionShape2D
 
-export var checkpoint_id := 1
-export var path_index := 1
-onready var editor_area = $Area2D
-onready var editor_collision = $Area2D/CollisionShape2D
+onready var top_part = $Top
+onready var middle_part = $Middle
+onready var bottom_part = $Bottom
 
-var type = "Checkpoint"
+var type = "Breakable"
 
 func collide(col_area):
-	if col_area.name == "CharArea":
-		GlobalVars.checkpoint_id = checkpoint_id
+	if col_area.name == "CharArea" and mode == 0:
+		var character = col_area.get_node("../")
+		character.kill()
 
 func _ready():
 	area.connect("area_entered", self, "collide")
