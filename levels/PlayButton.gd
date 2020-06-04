08187507c596f1b2d4eea2a0ecf1187a42cf5e03
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-06-04 19:27:41 -0400

    add some useless rotation effects

--- levels/PlayButton.gd
+++ levels/PlayButton.gd
@@ -3,10 +3,21 @@ extends Button
 export var button_type := 0
 
 var down = false
+var hovering = false
+
+var tick = 0
 
 func _ready():
 	connect("button_down", self, "down")
 	connect("button_up", self, "up")
+	connect("mouse_entered", self, "hover")
+	connect("mouse_exited", self, "unhover")
+
+func hover():
+	hovering = true
+	
+func unhover():
+	hovering = false
 
 func down():
 	down = true
@@ -21,3 +32,11 @@ func click():
 		GlobalVars.enter_play_mode()
 	elif button_type == 1:
 		GlobalVars.enter_editor_mode()
+
+func _process(delta):
+	if hovering:
+		tick += delta
+		rect_rotation = sin(tick * 2) * 20
+	else:
+		tick = 0
+		rect_rotation = 0
